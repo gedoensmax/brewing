6419c980d50f4652ad70c5474fb4cfa4336b4dc0
maxmueller <maxi.mueller@me.com>
2019-11-17 16:03:34 +0200

    pi data publish

--- monitor/publish_data_pi/main.py
+++ monitor/publish_data_pi/main.py
@@ -1,22 +1,38 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python3
+
+from w1thermsensor import W1ThermSensor
 import time
+import paho.mqtt.client as mqtt
 
-# GPIO SETUP
-channel = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+outside = W1ThermSensor(sensor_id='00000b2aa601')
+bottom_plate = W1ThermSensor(sensor_id='00000b2a90e2')
+inside =W1ThermSensor(sensor_id='00000b2ab1d5')
 
+BROKER_ADDRESS = "192.168.1.2"
+topic_bottom = 'brewie/temp/bottom'
+topic_inside = 'brewie/temp/inside'
+topic_outside = 'brewie/temp/outside'
 
-def callback(channel):
-    if GPIO.input(channel):
-        print("Water Detected!")
-    else:
-        print("Water Detected!")
+client = mqtt.Client('brewie')
+print("Connecting to broker")
+client.connect(host=BROKER_ADDRESS)
+client.loop_start()
 
-
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
-
-# infinite loop
 while True:
-    time.sleep(1)
+    try:
+        client.publish(topic_bottom, payload=bottom_plate.get_temperature())
+        print('Bottom plate: {}'.format(bottom_plate.get_temperature()))
+    except:
+        print("Failure in {}".format(topic_bottom))
+    try:
+        client.publish(topic_inside, payload=inside.get_temperature())
+        print('Inside case: {}'.format(inside.get_temperature()))
+    except:
+        print("Failure in {}".format(topic_inside))
+    try:
+        client.publish(topic_outside, payload=outside.get_temperature())
+        print('Waterm Temp: {}'.format(outside.get_temperature()))
+    except:
+        print("Failure in {}".format(topic_outside))
+    print('........................')
+    time.sleep(5)
